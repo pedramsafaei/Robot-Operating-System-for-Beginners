87e43131d8cc169d5d786cb24556ad3757d25c42
pedramsafaei <petersafaei@gmail.com>
2020-05-03 04:32:26 -0700

    simple if, else

---
+++ chapter3/robotMove/robot_move.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+# python 2
+#robot_command = raw_input("Enter the command:>  ")
+
+# python 3
+robot_command = input("Enter the command:>  ")
+
+if(robot_command == "move_left"):
+    print("Robot is moving Left")
+elif(robot_command == "move_right"):
+    print("Robot is moving right")
+elif(robot_command == "move_forward"):
+    print("Robot is moving forward")
+elif(robot_command == "move_backward"):
+    print(("Robot is moving backward"))
+else:
+    print("Invalid command")
