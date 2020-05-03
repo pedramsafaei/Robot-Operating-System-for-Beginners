087f93d13e8dde8fdac1fae2a2f3f8093a0a18d5
pedramsafaei <petersafaei@gmail.com>
2020-05-03 04:19:26 -0700

    basic python added

--- chapter3/hello_world.py
+++ chapter3/hello_world.py
@@ -2,13 +2,13 @@
 # -*- coding: utf-8 -*-
 
 
-__author__ = "Lentin Joseph"
-__copyright__ = "Copyright 2017, The Hello World Project"
-__credits__ = ["Apress"]
+__author__ = "Pedram Safaei"
+__copyright__ = "Copyright 2020, The Hello World Project"
+__credits__ = ["ROS"]
 __license__ = "GPL"
 __version__ = "0.0.1"
-__maintainer__ = "Lentin Joseph"
-__email__ = "qboticslabs@gmail.com"
+__maintainer__ = "Pedram Safaei"
+__email__ = "psafaei@nevada.unr.edu"
 __status__ = "Development"
 
 
