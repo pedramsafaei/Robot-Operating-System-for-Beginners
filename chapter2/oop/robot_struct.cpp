577df5a833d6d1980819ad37233ce2e87af47aa3
pedramsafaei <petersafaei@gmail.com>
2020-05-02 20:16:02 -0700

    format issue

--- chapter2/oop/robot_struct.cpp
+++ chapter2/oop/robot_struct.cpp
@@ -5,35 +5,35 @@ using namespace std;
 
 struct Robot_Struct
 {
-	int id;
-	int no_wheels;
-	string robot_name;
+  int id;
+  int no_wheels;
+  string robot_name;
 };
 
 class Robot_Class
 {
 public:
-	int id;
-	int no_wheels;
-	string robot_name;
+  int id;
+  int no_wheels;
+  string robot_name;
 
-	void move_robot();
-	void stop_robot();
+  void move_robot();
+  void stop_robot();
 };
 
 void Robot_Class::move_robot()
 {
-	cout << "Moving Robot" << endl;
+  cout << "Moving Robot" << endl;
 }
 
 void Robot_Class::stop_robot()
 {
-	cout << "Stopping Robot" << endl;
+  cout << "Stopping Robot" << endl;
 }
 
 int main()
 {
-	/*
+  /*
 	
   if you use a struct or class pointer then you would need to use 
   -> operator 
@@ -44,22 +44,22 @@ int main()
   robot_2 -> robot_name = "Something Robot";
 
 	*/
-	Robot_Struct robot_1;
-	Robot_Class robot_2;
+  Robot_Struct robot_1;
+  Robot_Class robot_2;
 
-	robot_1.id = 2;
-	robot_1.robot_name = "Mobile robot";
+  robot_1.id = 2;
+  robot_1.robot_name = "Mobile robot";
 
-	robot_2.id = 3;
-	robot_2.robot_name = "Humanoid robot";
+  robot_2.id = 3;
+  robot_2.robot_name = "Humanoid robot";
 
-	cout << "ID=" << robot_1.id << "\t"
-			 << "Robot Name: " << robot_1.robot_name << endl;
-	cout << "ID=" << robot_2.id << "\t"
-			 << "Robot Name: " << robot_2.robot_name << endl;
+  cout << "ID=" << robot_1.id << "\t"
+       << "Robot Name: " << robot_1.robot_name << endl;
+  cout << "ID=" << robot_2.id << "\t"
+       << "Robot Name: " << robot_2.robot_name << endl;
 
-	robot_2.move_robot();
-	robot_2.stop_robot();
+  robot_2.move_robot();
+  robot_2.stop_robot();
 
-	return 0;
+  return 0;
 }
